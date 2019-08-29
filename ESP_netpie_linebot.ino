827019e4df07627e5d725f9854bd63047ad5846a
0oluminoso0 <54668122+0oluminoso0@users.noreply.github.com>
2019-08-29 21:34:15 +0700

    Update ESP_netpie_linebot.ino

--- ESP_netpie_linebot.ino
+++ ESP_netpie_linebot.ino
@@ -2,13 +2,13 @@
 #include <MicroGear.h>
 #include <ESP8266HTTPClient.h>
 #include <ArduinoJson.h>
-const char* ssid     = "your SSID"; //change this to your SSID
-const char* password = "your PASSWORD"; //change this to your PASSWORD
+const char* ssid     = "NightOwl"; //change this to your SSID
+const char* password = "xy234786"; //change this to your PASSWORD
 
-const char* host = "your linebot server";//change this to your linebot server ex.http://numpapick-linebot.herokuapp.com/bot.php
-#define APPID   "your APPID"     //change this to your APPID
-#define KEY     "your KEY"     //change this to your KEY
-#define SECRET  "your SECRET"     //change this to your SECRET
+const char* host = "https://linebotdsp.herokuapp.com/bot.php";//change this to your linebot server ex.http://numpapick-linebot.herokuapp.com/bot.php
+#define APPID   "8VMmn1xyk2qlht0:iqGxfUPuHxCLlbEeeimCPi5ay"     //change this to your APPID
+#define KEY     "8VMmn1xyk2qlht0"     //change this to your KEY
+#define SECRET  "iqGxfUPuHxCLlbEeeimCPi5ay"     //change this to your SECRET
 
 #define ALIAS   "NodeMCU1" //set name of drvice
 #define TargetWeb "switch" //set target name of web
